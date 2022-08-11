33f0f85e4cb4fd3daeca554331b2e38d491af27b
Carlos Adrian <Teshynil@users.noreply.github.com>
2022-08-10 23:11:24 -0500

    update

--- assets/config.js
+++ assets/config.js
@@ -14,34 +14,16 @@ let config = {
       "icon_classes": "fab fa-discord",
     },
     {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
+      "Title": "Facebook",
+      "URL": "https://www.facebook.com/CentralDungeon",
+      "icon_classes": "fab fa-facebook"
     },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
-      "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
-    },
+    /*
     {
       "Title": "Youtube",
       "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
       "icon_classes": "fab fa-youtube"
     },
+    */
   ]
 }
